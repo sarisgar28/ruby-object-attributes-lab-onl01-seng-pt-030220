79f1f0f00753e60f3b769cac908d3eda984b1722
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-22 21:46:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog 
+  
+  #def initialize(name, breed)
+    #@name = name 
+    #@breed = breed 
+  #end
+  
+  def name 
+    return @name 
+  end
+  
+  def name=(n)
+    @name = n  
+  end 
+  
+  def breed 
+    @breed 
+  end 
+   
+   def breed=(b)
+     @breed = b 
+   end 
+   
+end 
+
+
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person 
+ 
+ # def initialize(name,job)
+  #  @name = name 
+   # @job = job 
+  #end 
+   
+  def name 
+    @name 
+  end 
+  
+  def name=(s)
+    @name = s 
+  end 
+  def job 
+    @job 
+  end 
+  def job=(j)
+    @job = j
+  end 
+end
